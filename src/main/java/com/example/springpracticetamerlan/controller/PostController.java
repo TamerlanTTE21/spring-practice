e620b585cbb049036e69f7c60a48f2bf8cf7708b
runtime-1 <runtimestudent2023@gmail.com>
2026-03-30 10:27:25 +0500

    s

--- src/main/java/com/example/springpracticetamerlan/controller/PostController.java
+++ src/main/java/com/example/springpracticetamerlan/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.springpracticetamerlan.controller;
 
 import com.example.springpracticetamerlan.model.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +13,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class PostContoller {
+public class PostController {
+    private static final Logger log = LoggerFactory.getLogger(PostController.class);
 
 private final List<Post> posts = new ArrayList<>();
 
     @GetMapping("/posts")
+
     public List<Post> getPosts() {
+        log.info("Текущее количество постов: " + posts.size());
         return posts;
     }
 
--- src/main/java/com/example/springpracticetamerlan/controller/UserController.java
+++ src/main/java/com/example/springpracticetamerlan/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.springpracticetamerlan.controller;
 
 import com.example.springpracticetamerlan.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +14,21 @@ import java.util.Map;
 
 @RestController
 public class UserController {
+//    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    private final Map<User, User> users = new HashMap<>();
+    private final Map<String, User> users = new HashMap<>();
 
     @GetMapping("/users")
-    public Map<User, User> getUsers() {
+    public Map<String, User> getUsers() {
+
+
+//        log.info("Текущее количество постов: ");
         return users;
     }
 
-    @PostMapping("/users")
-    public User create(@RequestBody User user) {
-        users.put(user, user);
-        return user;
-    }
+//    @PostMapping("/users")
+//    public User create(@RequestBody User user) {
+//        users.put(user, user);
+//        return user;
+//    }
 }
